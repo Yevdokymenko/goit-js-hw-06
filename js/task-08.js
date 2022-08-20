aa8ccbe723e6268d97b615cf8627c64b19e3becc
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2022-08-20 09:22:12 +0300

    add hw

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
 
+function checkSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Не огорчай, все поля заполняй!");
+  }
+
+  const result = { email: email.value, password: password.value };
+  console.log(result);
+  event.currentTarget.reset();
+}
+
+formEl.addEventListener("submit", checkSubmit);

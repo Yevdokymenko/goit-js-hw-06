e6418e5f2d779170bf03070ef24c3f87c7042517
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2022-08-25 11:17:05 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -10,6 +10,7 @@ const colorEl = document.querySelector(".color");
 changeColorEl.addEventListener("click", changeBgColorRandom);
 
 function changeBgColorRandom() {
-  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
-  colorEl.textContent = getRandomHexColor();
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color;
 }
